5b3af9aa56f751f37f998ddc3665bf1f6f896a78
JorgeMataH <matahj@gmail.com>
2022-04-20 21:35:09 -0500

    Se cambión una imagen

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'English Grammar',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('@site/static/img/file-earmark-font.svg').default,
     description: (
       <>
         Ayuda en el aprendizaje de la gramática básica del idioma inglés.
